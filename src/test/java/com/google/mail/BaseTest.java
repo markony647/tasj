ab32734f2e4df6a85dad15f29d4ae060b8a71076
Valentin Timchenko <vtimchenko87@gmail.com>
2016-05-04 22:01:08 +0300

    commit to another branch

--- src/test/java/com/google/mail/BaseTest.java
+++ src/test/java/com/google/mail/BaseTest.java
@@ -3,7 +3,6 @@ package com.google.mail;
 import com.google.mail.core.Configuration;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static com.google.mail.core.ConciseAPI.getWebDriver;
@@ -12,13 +11,9 @@ import static com.google.mail.core.ConciseAPI.setWebDriver;
 
 public class BaseTest {
 
-    @BeforeClass
-    public static void getDriver() {
-        setWebDriver(new FirefoxDriver());         // Инстанс драйвера создаю здесь , или всё таки лучше вынести в поле Configuration класса ?
-    }
-
     @Before
     public void setup() {
+        setWebDriver(new FirefoxDriver());         // Инстанс драйвера создаю здесь , или всё таки лучше вынести в поле Configuration класса ?
         Configuration.timeout = 20;
     }
 
@@ -26,5 +21,4 @@ public class BaseTest {
     public void tearDown() {
         getWebDriver().quit();
     }
-
 }
