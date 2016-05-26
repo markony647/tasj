1df01e190d7282f492451e388b65740ce01f25cf
Valentin Timchenko <vtimchenko87@gmail.com>
2016-05-26 20:43:43 +0300

    implemantation after review

--- src/main/java/com/google/mail/pages/Gmail.java
+++ src/main/java/com/google/mail/pages/Gmail.java
@@ -1,12 +1,10 @@
 package com.google.mail.pages;
 
-import com.google.mail.core.conditions.ElementConditions;
 import org.openqa.selenium.Keys;
 
 import static com.google.mail.core.ConciseAPI.$;
 import static com.google.mail.core.ConciseAPI.open;
 import static com.google.mail.core.ConciseAPI.setValue;
-import static com.google.mail.core.conditions.ElementConditions.presenceOfElement;
 
 
 public class Gmail {
@@ -17,9 +15,7 @@ public class Gmail {
     }
 
     public static void login(String email, String password) {
-        setValue($( "#Email.."), email + Keys.ENTER);
-
-        //setValue($("#Email"), email + Keys.ENTER);
+        setValue($("#Email"), email + Keys.ENTER);
         setValue($("#Passwd"), password + Keys.ENTER);
     }
 }
